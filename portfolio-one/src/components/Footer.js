da08a070bb608df1135e5d4e71a38aa037745001
ElijahBcodes <eli1inthesnow@gmail.com>
2022-09-11 21:38:29 -0700

    initial

---
+++ portfolio-one/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Footer.css"
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='socialMedia'>
+        <InstagramIcon/>
+        <TwitterIcon/>
+        <LinkedInIcon/>
+        <GithubIcon/>
+      </div>
+      <p> &copy;2022 elijahb.com</p>
+      
+      
+      </div>
+  )
+}
+
+export default Footer
