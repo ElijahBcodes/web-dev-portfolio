6c57c0619d10b22e442393a2fd4c8359f4ca8c1a
ElijahBcodes <eli1inthesnow@gmail.com>
2022-09-13 21:59:46 -0700

    projects

--- portfolio-one/src/helpers/ProjectList.js
+++ portfolio-one/src/helpers/ProjectList.js
@@ -1,10 +1,39 @@
+import Proj1 from "../assets/proj1.jpeg";
+import Proj2 from "../assets/proj2.jpeg";
+import Proj3 from "../assets/proj3.png";
+import Proj4 from "../assets/proj4.png";
+import Proj5 from "../assets/proj5.jpg";
+import Proj6 from "../assets/proj6.jpg"
 
-
-
-export const projectList= [
-{
-    name: 'Treasure Hunt'
-    image: Proj1
-}
-
-]
+export const ProjectList = [
+    {
+      name: "Tic Tac Toe",
+      image: Proj1,
+      tech: "JavaScript,HTML,CSS",
+    },
+    {
+      name: "Pig Latin Translator",
+      image: Proj2,
+      tech: "React, JavaScript",
+    },
+    {
+      name: "Treasure Hunt Game",
+      image: Proj3,
+      tech: "HTML, CSS, JavaScript",
+    },
+    {
+      name: "City of Larendale text Game",
+      image: Proj4,
+      tech: "Ruby",
+    },
+    {
+      name: "Blog App",
+      image: Proj5,
+      skills: "Ruby, Rails, ",
+    },
+    {
+      name: "Dice Game ",
+      image: Proj6,
+      skills: "React,JavaScript,HTML,CSS",
+    },
+  ];
